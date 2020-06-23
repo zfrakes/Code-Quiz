364d3fa4cd21a474a8df6b54c3b660a578fce0b4
Zoey Frakes <zfrakes811@gmail.com>
2020-06-23 18:38:01 -0400

    updated questions

--- questions.js
+++ questions.js
@@ -1,28 +1,28 @@
 var questions = [
     {
-      title: "What famous TV reality star was married for 72 days?",
-      choices: ["Beyonce", "Kim Kardashian", "Mariah Carey", "Pairs Hilton"],
-      answer: "Kim Kardashian"
+      title: "Which famous basketball player died in January 2020?",
+      choices: ["Michael Jordan", "Kobe Bryant", "LeBron James", "Kevin Durant"],
+      answer: "Kobe Bryant"
     },
     {
-      title: "What popular HBO TV series ended on May 19, 2019?",
-      choices: ["True Blood", "Girls", "Game of Thrones", "West World"],
-      answer: "Game of Thrones"
+      title: "What is the name for the ofical Gobal Pandemic?",
+      choices: ["Bird Flu", "Coronavirus", "Covid-19", "Spanish Flu"],
+      answer: "Covid-19"
     },
     {
-      title: "Which social media platform was launched in 2010?",
-      choices: ["Instagram", "Facebook", "Twitter", "Snapchat"],
-      answer: "Instagram"
+      title: "What is the name of giant killer insects spotted in the U.S?",
+      choices: ["Murder Hornets", "Bad Bees", "Killer Butterflies", "Bloodsucker Ladybugs"],
+      answer: "Murder Hornets"
     },
     {
-      title: "Which music artist released a song titled Bodak Yellow?",
-      choices: ["Taylor Swift", "Kodak Black", "Drake", "Cardi B"],
-      answer: "Cardi B"
+      title: "Which royal couple set down from their royal duties?",
+      choices: ["Prince William and Kate Middleton", "Princess Beatrice and Edoardo Mapelli Mozzi", "Princess Eugenie and Jack Brooksbank", "Prince Harry and Meghan Markle"],
+      answer: "Prince Harry and Meghan Markle "
     },
     {
-      title: "What year was the Women's March created?",
-      choices: ["2016", "2018", "2017", "2015"],
-      answer: "2017"
+      title: "Which country offically withdrew from the European Union?",
+      choices: ["France", "Spain", "U.K", "Germany"],
+      answer: "U.K"
     }
   ];
   
